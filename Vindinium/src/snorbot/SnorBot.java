f984c2427b49d33445f3d265db521d4f2ded350d
egmond-van-jos <jostievanegmond@gmail.com>
2014-05-25 11:20:22 +0200

    Redistributed responsibilities to separate classes. SnorBot is in charge of utilizing different strategies. PathMap contains a map with TileInfo classes for all positions on the board, and lists of special sites. TileInfo is a class that contains information about a Tile (distance in that turn, shortest path).

--- Vindinium/src/snorbot/SnorBot.java
+++ Vindinium/src/snorbot/SnorBot.java
@@ -3,6 +3,7 @@ package snorbot;
 import java.awt.Point;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 import java.util.Vector;
 
 import vindinium.Board.Tile;
@@ -13,18 +14,14 @@ import vindinium.State;
 
 public class SnorBot implements Bot
 {
+	private PathMap pathMap;
+	
 	private List<Hero> heroList;
-	private Tile[][] tileMatrix;
 	private Direction direction;
+	private Tile[] neighbourTiles;
 	
 	private Hero hero;
-	private Point position = new Point();
-	
-	private static Direction intToDir( int i )
-	{
-		Direction[] directions = { Direction.NORTH, Direction.EAST, Direction.SOUTH, Direction.WEST };
-		return directions[i];
-	}
+	private Point position;
 	
 	@Override
 	public Direction nextMove(State state)
@@ -39,102 +36,17 @@ public class SnorBot implements Bot
 		
 		this.direction = Direction.STAY;
 		this.hero = state.hero();
-		this.position.x = this.hero.position.right;
-		this.position.y = this.hero.position.left;
+		this.position = new Point( this.hero.position.right, this.hero.position.left ); //x and y are reversed, really
 		
-		this.heroList = state.game.heroes;
-		this.tileMatrix = state.game.board.tiles;
+		this.pathMap = new PathMap( state.game.board.tiles, this.position.x, this.position.y );
 
-		//see if there are free mines
-		if( attemptMine() == false )
-		{
-			//if not, find rich heroes
-			if( findRichHero( 10 ) == false )
-			{
-				//no rich heroes, move randomly
-				moveRandom();
-			}
-			
-			//rich hero found!
-			else
-			{
-				//kill the hero
-				killHero( this.hero );
-			}
-		}
-	
-		System.out.println( " - pos[" + this.position.x + "," + this.position.y + "]" );
+		//see if there are free mines in the direct vicinity
+		//if not, see if we need a drink
+		//if not, find rich heroes
+		//no rich heroes, move randomly
+		//rich hero found!
+		//kill the hero
+		
 		return this.direction;
 	}
-	
-	private boolean findRichHero( int gold )
-	{
-		for( Hero hero : heroList  )
-		{
-			if( hero != this.hero && hero.gold > gold )
-			{
-				return true; //eeew
-			}
-		}
-		
-		return false;
-	}
-	
-	private void killHero( Hero hero )
-	{
-		//if we can't mine this turn, move to hero
-		if( attemptMine() == false )
-		{
-			moveToHero( hero );
-		}
-	}
-	
-	private void moveToHero( Hero hero )
-	{
-		//shortest path to the hero!
-		//auto kills when next to player
-			
-		direction = intToDir( (int)(Math.random() * 4) );
-		System.out.print( "Moving to rich bastard" );
-	}
-	
-	private void moveRandom()
-	{
-		direction = intToDir( (int)(Math.random() * 4) );
-		System.out.print( "Randomly wander around" );
-	}
-	
-	private boolean attemptMine()
-	{
-		boolean canMine = false;
-		Tile[] neighbourTiles = getNeighbourTiles( this.position.x, this.position.y );
-
-		for( int i = 0; i < 4 && canMine == false; i++ )
-		{
-			Tile tile = neighbourTiles[i];
-			if( tile == Tile.FREE_MINE )
-			{
-				canMine = true;
-				System.out.print( "Can mine! Mine is in the " + intToDir(i).toString() );
-				direction = intToDir( i );
-			}
-		}
-		
-		return canMine;
-	}
-	
-	private Tile[] getNeighbourTiles( int x, int y )
-	{ 
-		int xMax = tileMatrix[0].length;
-		int yMax = tileMatrix.length;
-		
-		Tile[] tiles = new Tile[4];
-		
-		tiles[0] = ( y - 1 >= 0 ) ? this.tileMatrix[ y - 1 ][ x ] : Tile.WALL;		//NORTH
-		tiles[1] = ( x + 1 < xMax ) ? this.tileMatrix[ y ][ x + 1 ] : Tile.WALL;	//EAST
-		tiles[2] = ( y + 1 < yMax ) ? this.tileMatrix[ y + 1 ][ x ] : Tile.WALL;	//SOUTH
-		tiles[3] = ( x - 1 >= 0 ) ? this.tileMatrix[ y ][ x - 1 ] : Tile.WALL;		//WEST
-		
-		return tiles;
-	};
 }
